7c17aeed031b3b710762b2b97d5732e1c8e2d6a6
felipe mascena <felipe.mascena.dev@gmail.com>
2025-03-04 02:22:53 -0300

    feat: Header finalizado

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,6 +8,8 @@ export default function Header() {
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/cardapio'>Cardápio</NavLink>
         <NavLink to='/para_empresas'>Para Empresas</NavLink>
+        </nav>
+        <nav>
         <NavLink to='/seu_evento'>Faça seu Evento</NavLink>
         <NavLink to='/reservas'>Reservas</NavLink>
         <NavLink to='/localizacao'>Localização</NavLink>
