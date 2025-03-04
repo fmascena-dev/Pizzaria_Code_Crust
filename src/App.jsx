34306f4855640198a13f527513d9c69a02062b69
felipe mascena <felipe.mascena.dev@gmail.com>
2025-03-04 01:37:46 -0300

    feat: Iniciando a Home e Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import AOS from 'aos';
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+
 export default function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+      mirror: true,
+    });
+  }, []);
+
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
