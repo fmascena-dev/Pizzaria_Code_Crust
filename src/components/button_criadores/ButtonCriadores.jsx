b09d248fb9598a6ad52b3fefcd7ea951322d5b7f
felipe mascena <felipe.mascena.dev@gmail.com>
2025-03-04 21:34:41 -0300

    feat: Home finalizada

--- src/components/button_criadores/ButtonCriadores.jsx
+++ src/components/button_criadores/ButtonCriadores.jsx
@@ -1,7 +1,8 @@
+import './ButtonCriadoresStyle.scss'
 import { useNavigate } from 'react-router-dom';
 
 export default function ButtonCriadores() {
   const navigate = useNavigate();
 
-  return <button onClick={() => navigate('/criadores')}>Criadores</button>;
+  return <button onClick={() => navigate('/criadores')} className="button-criadores">Criadores</button>;
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,82 @@
+import '../../styles/_home.scss'
+import Logo from '../../assets/home/Logo.png';
+import ImgTwo from '../../assets/home/pizza1.jpg'
+import 'aos/dist/aos.css';
+
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <main class="home">
+      <section class="inicio">
+        <h1>Seja muito bem-vindo à Pizzaria</h1>
+        <img src={Logo} class="logo" alt="Logo Code&Crust" />
+      </section>
+      <section class="historia">
+        <h2>Conheça a Nossa História</h2>
+        <div class="paragrafos">
+          <p data-aos="fade-right">
+            No coração de uma cidade vibrante, onde o amor pela tecnologia se
+            encontrava com a paixão pela gastronomia, nasceu a Code&Crust
+            Pizzaria. A história começa com dois amigos de infância, Lucas e
+            Ana, que sempre sonharam em criar um espaço onde pudessem unir suas
+            duas grandes paixões: a programação e a culinária.
+          </p>
+          <p data-aos="fade-left">
+            Lucas, um talentoso desenvolvedor de software, sempre passava horas
+            criando códigos e aplicativos em seu computador. Ana, por outro
+            lado, era uma cozinheira excepcional, conhecida por suas receitas de
+            pizza irresistíveis, que faziam todos na vizinhança voltarem por
+            mais. Durante longas noites de programação, os dois se reuniam para
+            compartilhar suas ideias, e foi em uma dessas noites que surgiu a
+            ideia da Code&Crust.
+          </p>
+          <p data-aos="fade-right">
+            A inspiração veio quando Lucas, em uma de suas madrugadas de
+            programação, teve um insight sobre como poderia usar a tecnologia
+            para melhorar a experiência de pedir pizzas. Ele começou a desenhar
+            um aplicativo que permitisse aos clientes personalizar suas pizzas
+            de maneira interativa, escolhendo ingredientes, tamanhos e até mesmo
+            a crocância da massa. Ana, animada com a ideia, começou a
+            experimentar novas receitas, combinando sabores tradicionais com
+            toques modernos, sempre com o objetivo de oferecer pizzas de alta
+            qualidade.
+          </p>
+          <p data-aos="fade-left">
+            Com o plano em mente, eles decidiram abrir a
+            <strong>Code&Crust Pizzaria</strong>, um lugar onde a tecnologia e a
+            culinária se encontrariam. O conceito era simples: cada pizza seria
+            uma obra de arte, assim como um código bem escrito. O nome
+            "Code&Crust" simbolizava essa união entre a programação e a massa
+            crocante das pizzas. Logo, a pizzaria se tornou um sucesso. Os
+            clientes adoravam o aplicativo que Lucas desenvolveu, que não apenas
+            permitia a personalização das pizzas, mas também oferecia sugestões
+            baseadas em suas preferências anteriores. Enquanto isso, Ana
+            continuava a inovar no cardápio, trazendo sabores de diferentes
+            partes do mundo para suas criações.
+          </p>
+          <p data-aos="fade-right">
+            O ambiente da Code&Crust era acolhedor, decorado com referências ao
+            mundo da tecnologia, como murais de código e ilustrações de pizzas.
+            A equipe era composta por amantes da tecnologia e da culinária, que
+            compartilhavam a mesma visão de oferecer uma experiência única aos
+            clientes. Com o passar dos anos, a Code&Crust Pizzaria se expandiu,
+            abrindo filiais em outras cidades e sempre mantendo o compromisso
+            com a qualidade e a inovação. Lucas e Ana continuaram a trabalhar
+            juntos, inspirando-se mutuamente e criando uma comunidade de pessoas
+            que não apenas amavam pizza, mas também a tecnologia.
+          </p>
+          <p data-aos="fade-left">
+            Hoje, a <strong>Code&Crust Pizzaria</strong> é mais do que apenas um
+            lugar para comer; é um espaço onde as pessoas se reúnem para
+            celebrar a vida, a amizade e a paixão por sabores e códigos. E
+            assim, a história de origem da Code&Crust continua a ser escrita,
+            com cada pizza servida sendo uma nova linha de código na deliciosa
+            história que os amigos construíram juntos.
+          </p>
+        </div>
+      </section>
+      <section class="img-final" data-aos="fade-right">
+        <img src={ImgTwo} alt="" />
+      </section>
+    </main>
     )
 };
